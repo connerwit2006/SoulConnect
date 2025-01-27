0a60b5d0023f0e786cb560aea17d175d3b889789
Govert Jansen <97050782@st.deltion.nl>
2025-01-27 11:34:43 +0100

    Components created for detail page

---
+++ resources/views/components/online-indicator.blade.php
@@ -0,0 +1,10 @@
+<!-- Online Indicator-->
+<div>
+    <div class="flex justify-center">
+        <span class="relative flex h-3 w-3">
+            <span class="absolute inline-flex h-full w-full animate-ping-slow rounded-full bg-green-400 opacity-75"></span>
+            <span class="relative inline-flex h-3 w-3 rounded-full bg-green-500"></span>
+        </span>
+    </div>
+    <span class="text-green-500">Online</span>
+</div>
